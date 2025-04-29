6f20ba808334337ba3a5b13d9e40f4fb5d69a5eb
floyd <floyd.zweydinger@rub.de>
2025-04-29 21:14:55 +0200

    gitbak

--- lll_rs/src/math/number.rs
+++ lll_rs/src/math/number.rs
@@ -1,68 +1,104 @@
 use rug::{Complete, Float, Integer};
 use std::{
-    borrow::Borrow, cmp::PartialOrd, fmt::{Debug, Display}, iter::Sum, ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign}
+    borrow::Borrow,
+    cmp::PartialOrd,
+    fmt::{Debug, Display},
+    iter::Sum,
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign}
 };
 
-pub trait BigNumber<'a>
-where
-    Self: Sized,
-    Self: Default,
-    Self: Add<Self>,
-    &'a Self:Add<&'a Self>,
-    // TODO Self: Display,
-    Self: Clone, // TODO remove
-{
-    // fn rand_normal(n: usize, exp_from: i32, exp_to: i32, gs: G, sign_positive: bool) -> Vec<Self>;
-    fn new() -> Self;
-    fn add(&self, rhs: &Self) -> Self;
-    fn sub(&self, rhs: &Self) -> Self;
-    fn mul(&self, rhs: &Self) -> Self;
-    fn div(&self, rhs: &Self) -> Self;
-}
+pub trait BigNumber:
+    From<i32>
+    + PartialEq
+    + PartialOrd<Self>
+    + Clone
+    + Debug
+    + Default
+    + for<'a> Add<&'a Self, Output = Self>
+    + for<'a> AddAssign<&'a Self>
+    + for<'a> Sub<&'a Self, Output = Self>
+    + for<'a> SubAssign<&'a Self>
+    + for<'a> Mul<&'a Self, Output = Self>
+    + Sum<Self>
+{}
 
+impl<T> BigNumber for T where T:
+    From<i32>
+    + PartialEq
+    + PartialOrd<Self>
+    + Clone
+    + Debug
+    + Default
+    + for<'a> Add<&'a Self, Output = Self>
+    + for<'a> AddAssign<&'a Self>
+    + for<'a> Sub<&'a Self, Output = Self>
+    + for<'a> SubAssign<&'a Self>
+    + for<'a> Mul<&'a Self, Output = Self>
+    + Sum<Self>
+{}
 
-#[derive(Clone, Default)]
-pub struct BigInteger(Integer);
+pub type BigInteger = Integer;
 
-
-impl BigNumber for BigInteger {
-    fn new() -> Self {
-        Self(Integer::new())
-    }
-
-    fn add(&self, rhs: &Self) -> Self {
-        Self((self.0.borrow() + rhs.0.borrow()).complete())
-    }
-
-    fn sub(&self, rhs: &Self) -> Self {
-        Self((self.0.borrow() - rhs.0.borrow()).complete())
-    }
-
-    fn mul(&self, rhs: &Self) -> Self {
-        Self((self.0.borrow() * rhs.0.borrow()).complete())
-    }
-
-    fn div(&self, rhs: &Self) -> Self {
-        Self((self.0.borrow() / rhs.0.borrow()).complete())
-    }
-}
-
-impl Add for BigInteger {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self {
-        BigNumber::add(&self, &rhs)
-    }
-}
-
-impl Add for &BigInteger {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self {
-        BigNumber::add(self, rhs);
-        &self
-    }
-}
+//pub trait BigNumber<'a>
+//where
+//    Self: Sized,
+//    Self: Default,
+//    Self: Add<Self>,
+//    &'a Self:Add<&'a Self>,
+//    // TODO Self: Display,
+//    Self: Clone, // TODO remove
+//{
+//    // fn rand_normal(n: usize, exp_from: i32, exp_to: i32, gs: G, sign_positive: bool) -> Vec<Self>;
+//    fn new() -> Self;
+//    fn add(&self, rhs: &Self) -> Self;
+//    fn sub(&self, rhs: &Self) -> Self;
+//    fn mul(&self, rhs: &Self) -> Self;
+//    fn div(&self, rhs: &Self) -> Self;
+//}
+//
+//
+//#[derive(Clone, Default)]
+//pub struct BigInteger(Integer);
+//
+//
+//impl BigNumber for BigInteger {
+//    fn new() -> Self {
+//        Self(Integer::new())
+//    }
+//
+//    fn add(&self, rhs: &Self) -> Self {
+//        Self((self.0.borrow() + rhs.0.borrow()).complete())
+//    }
+//
+//    fn sub(&self, rhs: &Self) -> Self {
+//        Self((self.0.borrow() - rhs.0.borrow()).complete())
+//    }
+//
+//    fn mul(&self, rhs: &Self) -> Self {
+//        Self((self.0.borrow() * rhs.0.borrow()).complete())
+//    }
+//
+//    fn div(&self, rhs: &Self) -> Self {
+//        Self((self.0.borrow() / rhs.0.borrow()).complete())
+//    }
+//}
+//
+//impl Add for BigInteger {
+//    type Output = Self;
+//
+//    fn add(self, rhs: Self) -> Self {
+//        BigNumber::add(&self, &rhs)
+//    }
+//}
+//
+//impl Add for &BigInteger {
+//    type Output = Self;
+//
+//    fn add(self, rhs: Self) -> Self {
+//        BigNumber::add(self, rhs);
+//        &self
+//    }
+//}
 
 #[cfg(test)]
 mod tests {
